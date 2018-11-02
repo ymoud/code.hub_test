fa245a9dc3dbd04a08714b706f7d63273768b45d
Yannis Moudilos <Yannis@YANNIS-LAPTOP>
2018-11-02 20:45:46 +0200

    Updates

---
+++ precache-manifest.0fa9d396a5e8d62b6145bcc780e7f80e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/code.hub_test/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "85686d5914903a1eb8ca",
+    "url": "/code.hub_test/static/js/runtime~main.85686d59.js"
+  },
+  {
+    "revision": "a158d114d2178a2a4e8b",
+    "url": "/code.hub_test/static/js/main.a158d114.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/code.hub_test/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "a158d114d2178a2a4e8b",
+    "url": "/code.hub_test/static/css/main.38bb786b.chunk.css"
+  },
+  {
+    "revision": "0de3cbbce32c84018247a3fef799d0da",
+    "url": "/code.hub_test/index.html"
+  }
+];
